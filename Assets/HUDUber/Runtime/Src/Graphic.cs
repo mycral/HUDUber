4f8abea93da87b1553a491103784e87c5008527f
guoliulong <344756125@qq.com>
2024-06-28 00:08:25 +0800

    111

---
+++ Assets/HUDUber/Runtime/Src/Graphic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HUDUber
+{
+    public class Graphic
+    {
+        public Rect m_kRect;
+
+        public virtual void BuildMesh()
+        { }
+
+    }
+}
---
+++ Assets/HUDUber/Runtime/Src/Sprite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HUDUber
+{
+    public class Sprite
+    {
+        public Rect TexRect;
+        public Texture2D Texture;
+    }
+}
